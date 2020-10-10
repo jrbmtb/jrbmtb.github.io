ffcc1675b2719b84dc750e79a85023b598bbe7c3
jrbmtb <jacomtb97@gmail.com>
2020-10-10 12:26:46 -0500

    commit

--- lesson04/js/script.js
+++ lesson04/js/script.js
@@ -1,3 +1,13 @@
+// Function for hamburguer menu
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+
+// Get current date
+var currentDate = new Date();
+var dd = String(currentDate.getDate()).padStart(2, '0');
+var mm = String(currentDate.getMonth() + 1).padStart(2, '0'); 
+var yyyy = currentDate.getFullYear();
+
+var format = { weekday: 'long', day: 'numeric', year: 'numeric', month: 'long'};
+document.write(currentDate.toLocaleDateString('en', format));
