59526ed3ea3564df1302c2bd3f01f2a743381a43
jrbmtb <jacomtb97@gmail.com>
2020-10-15 12:41:52 -0500

    commit

--- lesson05/js/script.js
+++ lesson05/js/script.js
@@ -3,6 +3,20 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 
+// Pop-up message (Pancakes)
+const today = new Date();
+console.log(today);
+
+const dayNumber = today.getUTCDay();
+console.log(dayNumber);
+
+const element = document.getElementById("message");
+if (dayNumber == 5) {
+    element.classList.add("showme");
+} else {
+    element.classList.add("hideme");
+}
+
 // Get current date
 var currentDate = new Date();
 var dd = String(currentDate.getDate()).padStart(2, '0');
