68c1359c6c4dcc9e8d82c9b849f9acbe70a25b17
jrbmtb <jacomtb97@gmail.com>
2020-10-31 13:37:32 -0500

    commit

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -26,4 +26,35 @@ var yyyy = currentDate.getFullYear();
 var format = { weekday: 'long', day: 'numeric', year: 'numeric', month: 'long'};
 document.write(currentDate.toLocaleDateString('en', format));
 
+// Lazy Loading - Images Gallery Page
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+if('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
