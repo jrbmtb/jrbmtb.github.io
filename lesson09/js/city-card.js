f732cbdb532100ccf33b7dcabfd057554484bd0e
jrbmtb <jacomtb97@gmail.com>
2020-11-17 10:19:02 -0500

    commit

--- lesson09/js/city-card.js
+++ lesson09/js/city-card.js
@@ -14,24 +14,24 @@ fetch(requestURL)
     for (let i = 0; i < towns.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2'); //Town name
-        let p = document.createElement('p'); //Motto
-        let p2 = document.createElement('p'); //Year founded
-        let p3 = document.createElement('p'); //Population
-        let p4 = document.createElement('p'); //Avg rainfall
+        let h4 = document.createElement('h4'); //Motto
+        let p1 = document.createElement('p'); //Year founded
+        let p2 = document.createElement('p'); //Population
+        let p3 = document.createElement('p'); //Avg rainfall
         let img = document.createElement('img'); //Town photo
 
         h2.textContent = towns[i].name; //Town name
-        p.textContent = towns[i].motto; //Motto
-        p2.textContent = 'Year Founded: ' + towns[i].yearFounded; //Year founded
-        p3.textContent = 'Population: ' + towns[i].currentPopulation; //Population
-        p4.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall; //Avg rainfall
+        h4.textContent = towns[i].motto; //Motto
+        p1.textContent = 'Year Founded: ' + towns[i].yearFounded; //Year founded
+        p2.textContent = 'Population: ' + towns[i].currentPopulation; //Population
+        p3.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall; //Avg rainfall
         img.setAttribute('src', towns[i].photo, alt = towns[i].name); //Town photo
 
         card.appendChild(h2); //Town name
-        card.appendChild(p); //Motto
-        card.appendChild(p2); //Year founded
-        card.appendChild(p3); //Population
-        card.appendChild(p4); //Avg rainfall
+        card.appendChild(h4); //Motto
+        card.appendChild(p1); //Year founded
+        card.appendChild(p2); //Population
+        card.appendChild(p3); //Avg rainfall
         card.appendChild(img); //Town photo
 
         document.querySelector('div.cards').appendChild(card);
