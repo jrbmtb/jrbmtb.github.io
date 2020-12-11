2453212d7035c09b5a10f2a425cf9fb143698957
jrbmtb <jacomtb97@gmail.com>
2020-12-11 10:38:14 -0500

    rental page

--- scoots-final/js/rental-cards.js
+++ scoots-final/js/rental-cards.js
@@ -15,34 +15,39 @@ fetch(requestURL)
       let card = document.createElement('section');
       let img = document.createElement('img'); //Rental image
       let h2 = document.createElement('h2'); //Rental name
-      let p1 = document.createElement('p'); //Max persons
+      let p1 = document.createElement('h4'); //Max persons
+      let hr = document.createElement('hr'); //Divion
       let h3 = document.createElement('h3'); //Reservation $
       let p2 = document.createElement('p'); //Half day
       let p3 = document.createElement('p'); //Full day
       let h3s = document.createElement('h3'); //Walk-In $
       let p4 = document.createElement('p'); //Half day
       let p5 = document.createElement('p'); //Full day
+      let b = document.createElement('a'); //Rent button
 
       img.setAttribute('src', "images/" + rentals[i].picture); //Rental image
       img.setAttribute('alt', "Picture of " + rentals[i].rentalType);
       h2.textContent = rentals[i].rentalType; //Rental name
-      p1.textContent = 'Max. Persons: ' + rentals[i].maxPersons; //Max persons
-      h3.textContent = 'Reservation Price:'; //Reservation $
-      p2.textContent = 'Half day: ' + rentals[i].reservation.halfDay; //Half day
-      p3.textContent = 'Full day: ' + rentals[i].reservation.fullDay; //Full day
-      h3s.textContent = 'Walk-In Price: '; //Walk-In $
-      p4.textContent = 'Half day: ' + rentals[i].walkIn.halfDay; //Half day
-      p5.textContent = 'Full day: ' + rentals[i].walkIn.fullDay; //Full day
+      p1.textContent = 'MAX. PERSONS: ' + rentals[i].maxPersons; //Max persons
+      h3.textContent = 'Reservation Price'; //Reservation $
+      p2.textContent = 'Half day: $' + rentals[i].reservation.halfDay; //Half day
+      p3.textContent = 'Full day: $' + rentals[i].reservation.fullDay; //Full day
+      h3s.textContent = 'Walk-In Price'; //Walk-In $
+      p4.textContent = 'Half day: $' + rentals[i].walkIn.halfDay; //Half day
+      p5.textContent = 'Full day: $' + rentals[i].walkIn.fullDay; //Full day
+      b.textContent = 'I WANT THIS'; //Rent button
 
       card.appendChild(img); //Rental image
       card.appendChild(h2); //Rental name
       card.appendChild(p1); //Max persons
+      card.appendChild(hr); //Division
       card.appendChild(h3); //Reservation $
       card.appendChild(p2); //Half day
       card.appendChild(p3); //Full day
       card.appendChild(h3s); //Walk-In $
       card.appendChild(p4); //Half day
       card.appendChild(p5); //Full day
+      card.appendChild(b); //Rent button
 
       document.querySelector('div.rental-info').appendChild(card);
 
